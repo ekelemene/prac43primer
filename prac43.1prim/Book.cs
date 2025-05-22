2caa32623d5bb96f603075c19be74469c41e3b66
ekelemene <ksuxaskrip2007@gmail.com>
2025-05-22 10:31:55 +0300

    Add files via upload

---
+++ prac43.1prim/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac43._1prim
+{
+    public struct Book
+    {
+        public string Name;
+        public int Year;
+        public string Autor;
+
+
+
+
+    }
+}
---
+++ prac43.1prim/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac43._1prim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b;
+            b.Name = "BookName";
+            b.Year = 2000;
+            b.Autor = "BookAutor";
+
+            Console.WriteLine("НАзвание: "+b.Name);
+            Console.WriteLine("ГОд: " + b.Year);
+            Console.WriteLine("АВтор: " + b.Autor);
+            Console.Read();
+        }
+    }
+}
