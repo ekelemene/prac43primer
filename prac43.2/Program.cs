20d6235552ff673aba997bac38a95e9a26c14e36
ekelemene <ksuxaskrip2007@gmail.com>
2025-05-23 12:29:30 +0300

    Add files via upload

---
+++ prac43.2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac43._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод количества студентов
+            Console.Write("Введите количество студентов: ");
+            int n = int.Parse(Console.ReadLine());
+
+            //объявляем массив студентов размером в n
+            Student[] students = new Student[n];
+
+            //цикл с заполнением массива с клавиатуры
+            for (int i = 0; i < n; i++)
+            {
+                //вводим все данные
+                Console.WriteLine($"\nСтудент №{i + 1}");
+
+                Console.Write("ФИО: ");
+                students[i].fullName = Console.ReadLine();
+
+                Console.Write("Группа: ");
+                students[i].group = Console.ReadLine();
+
+                Console.Write("Оценка по информатике: ");
+                students[i].informatics = int.Parse(Console.ReadLine());
+
+                Console.Write("Оценка по физике: ");
+                students[i].physics = int.Parse(Console.ReadLine());
+
+                Console.Write("Оценка по истории: ");
+                students[i].history = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nСтуденты со средним баллом больше 4:");
+            int count = 0; //счетчик который подсчитает сколько студентов с баллом выше 4
+
+            for (int i = 0; i < n; i++) //цикл для проверки каждого студенкта 
+            {
+                double avg = students[i].Average(); //вызываем метод  для подсчета
+                if (avg > 4) //ставим условие  если больше 4 то выводим информацию о студенте и увеличиваем счетчик
+                {
+                    Console.WriteLine($"{students[i].fullName}, группа {students[i].group}, средний балл: {avg:F2}");
+                    count++;
+                }
+            }
+            if (count == 0) //если счетчи равен 0 то выводим сообщение, это означает что таких студентов нет
+            {
+                Console.WriteLine("Нет студентов со средним баллом выше 4.");
+            }
+            else //если есть то выводи сообщение с счетчиком 
+            {
+                Console.WriteLine($"\nКоличество таких студентов: {count}");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ prac43.2/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac43._2
+{
+    /// <summary>
+    /// структура студент
+    /// </summary>
+    public struct Student
+    {
+        /// <summary>
+        /// открытые поля фио группа информатика физика история
+        /// </summary>
+        public string fullName;
+        public string group;
+        public int informatics;
+        public int physics;
+        public int history;
+        /// <summary>
+        /// метод ср оценки выше 4 баллов
+        /// </summary>
+        /// <returns></returns>
+        public double Average()
+        {
+            return (informatics + physics + history) / 3.0;
+        }
+    }
+}
